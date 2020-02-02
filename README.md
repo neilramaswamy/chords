33622b444d2c20fc892cf26046475b06025fc82a
Neil Ramaswamy <neil@ramaswamy.org>
2020-02-02 12:31:35 -0500

    Simple CLI quizzer; Add project description to README

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# chords
-Proof-of-concept for an application that helps you learn the notes in common jazz chords. Because 570 is hard.
+# Chords
+An application that helps you learn the notes in common jazz chords. Because 570 is hard.
+
+Users will be prompted with a chord, given a chance to vocally dictate the notes, and then they will be told if their dictation was correct. I'm leaning toward vocal dictation (pitch ignorant, however) since speaking is faster than typing, and it more-closely parallels the type of recall needed for Jazz (or, at least, the type of recall that I'd like to have).
+
+Here's a more-concrete idea of what an interaction with this app will look like:
+
+- Front-end displays a chord like "A7#5"
+- Front-end pauses for a set amount of time then starts recording audio.
+- User dictates the notes that they think are in the chord: "A", "C#", "E#", "G".
+- Back-end verifies their answer and sends the result to the front-end, which tells the user how they did.
+
+This will likely turn into a web app whose frontend will be written in Typescript and whose backend will be written in Python. Each "end" will be running its own server. The Python ("verification" server, API) will use GCP's Speech-to-Text to turn audio into a string, which we can more-easily parse.
---
+++ api/checker.py
@@ -0,0 +1,50 @@
+from musthe import Note, Chord
+from typing import List
+from random import choice
+
+notes = ["A", "B", "C", "D", "E", "F", "G"]
+
+data = {
+    "major": "M",
+    "minor": "m",
+    "dominant 7": "dom7"
+}
+
+def are_notes_equal(n1: Note, n2: Note) -> bool:
+    """
+    Returns whether `n1` and `n2` are the same note, disregarding octave. Checks
+    note name and accidental values to determine this equality.
+    """
+    letters_equal = n1.letter == n2.letter
+    accidentals_equal = n1.accidental == n2.accidental
+
+    return letters_equal and accidentals_equal
+
+def are_chords_equal(c1: Chord, notes: List[str]) -> bool:
+    """
+    Returns whether two chords have the same notes in the same order, not 
+    considering octave.
+    """
+    chord_notes = c1.notes
+
+    if not len(chord_notes) is len(notes):
+        return False
+    
+    for i in range(len(chord_notes)):
+        if not are_notes_equal(chord_notes[i], Note(notes[i])):
+            return False
+
+    return True
+
+def quiz() -> Note:
+    note = choice(notes)
+    chord_name, chord_key = choice(list(data.items()))
+    print(chord_name, chord_key)
+
+    inp = input(f"Enter space-separated notes for {note} {chord_name}: ")
+    
+    target_chord = Chord(Note(note), chord_key)
+    submission = inp.split(' ')
+    is_correct = are_chords_equal(target_chord, submission)
+
+    print("You were right!") if is_correct else print("You were wrong.")
